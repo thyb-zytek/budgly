5a2e80fbb57ca2d41b6cb7079def2b31709c5ab2
Alexis Thibault <alexis.thibault31@gmail.com>
2025-04-14 21:28:32 +0200

    :lipstick: Add font family

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsBinding _ = WidgetsFlutterBinding.ensureInitialized();
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('assets/fonts/saira/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['assets/fonts/saira/'], license);
+  });
+
   runApp(const MyApp());
 }
 
