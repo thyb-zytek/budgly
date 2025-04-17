49f4106f5f6a35de6a8c28319993343d859cc539
Alexis Thibault <alexis.thibault31@gmail.com>
2025-04-17 23:21:55 +0200

    :sparkles: Create BottomNavBar

---
+++ lib/src/shared/bottom_navbar/bottom_navbar.dart
@@ -0,0 +1,45 @@
+import 'package:app/l10n/app_localizations.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class BottomNavBar extends StatefulWidget {
+  final StatefulNavigationShell child;
+
+  const BottomNavBar({super.key, required this.child});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    AppLocalizations tr = AppLocalizations.of(context)!;
+
+    return Scaffold(
+      body: SafeArea(child: widget.child),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: widget.child.currentIndex,
+        onTap: (index) {
+          widget.child.goBranch(
+            index,
+            initialLocation: index == widget.child.currentIndex,
+          );
+          setState(() {});
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart_rounded),
+            label: tr.overviewLink,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings_rounded),
+            label: tr.settingsLink,
+          ),
+        ],
+      ),
+    );
+    ;
+  }
+}
