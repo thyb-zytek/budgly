215037052580a2f9b8c1d2bc88c37d0338412d46
Alexis Thibault <alexis.thibault31@gmail.com>
2025-06-02 23:56:03 +0200

    :recycle: Update router to handle authentication

--- lib/src/core/routers/base.dart
+++ lib/src/core/routers/base.dart
@@ -1,6 +1,8 @@
+import 'package:app/src/pages/login/view.dart';
 import 'package:app/src/pages/overview/view.dart';
 import 'package:app/src/pages/settings/view.dart';
 import 'package:app/src/shared/widgets/bottom_navbar/bottom_navbar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -13,7 +15,7 @@ class NavigationHelper {
 
   static final GlobalKey<NavigatorState> parentNavigatorKey =
       GlobalKey<NavigatorState>();
-  static final GlobalKey<NavigatorState> homeTabNavigatorKey =
+  static final GlobalKey<NavigatorState> overviewTabNavigatorKey =
       GlobalKey<NavigatorState>();
   static final GlobalKey<NavigatorState> settingsTabNavigatorKey =
       GlobalKey<NavigatorState>();
@@ -26,24 +28,29 @@ class NavigationHelper {
   GoRouteInformationParser get routeInformationParser =>
       router.routeInformationParser;
 
+  static const String loginPath = '/login';
   static const String overviewPath = '/overview';
   static const String settingsPath = '/settings';
 
-  factory NavigationHelper() {
-    return _instance;
-  }
+  factory NavigationHelper() => _instance;
 
   NavigationHelper._internal() {
     final routes = [
+      GoRoute(
+        path: loginPath,
+        pageBuilder: (context, state) {
+          return getPage(child: const LoginPage(), state: state);
+        },
+      ),
       StatefulShellRoute.indexedStack(
         parentNavigatorKey: parentNavigatorKey,
         branches: [
           StatefulShellBranch(
-            navigatorKey: homeTabNavigatorKey,
+            navigatorKey: overviewTabNavigatorKey,
             routes: [
               GoRoute(
                 path: overviewPath,
-                pageBuilder: (context, GoRouterState state) {
+                pageBuilder: (context, state) {
                   return getPage(child: const OverviewPage(), state: state);
                 },
               ),
@@ -78,6 +85,16 @@ class NavigationHelper {
       navigatorKey: parentNavigatorKey,
       initialLocation: overviewPath,
       routes: routes,
+      redirect: (BuildContext context, GoRouterState state) async {
+        final bool loggedIn =
+            FirebaseAuth.instance.currentUser != null
+                ? FirebaseAuth.instance.currentUser!.emailVerified
+                : false;
+        final bool loggingIn = state.matchedLocation == loginPath;
+        if (!loggedIn) return loginPath;
+        if (loggingIn) return overviewPath;
+        return null;
+      },
     );
   }
 
