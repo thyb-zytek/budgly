b7fb987e683e9c952824d4f54025386cabc182d8
Alexis Thibault <alexis.thibault31@gmail.com>
2025-06-02 23:48:29 +0200

    :heavy_plus_sign: Add & setup Firebase authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:app/firebase_options.dart';
 import 'package:app/src/app.dart';
 import 'package:app/src/core/routers/base.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -11,7 +13,12 @@ void main() {
     yield LicenseEntryWithLineBreaks(['assets/fonts/saira/'], license);
   });
 
-  NavigationHelper.instance;
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then(
+    (_) {
+      NavigationHelper.instance;
 
-  runApp(const BudglyApp());
+      runApp(const BudglyApp());
+    },
+  );
+
 }
