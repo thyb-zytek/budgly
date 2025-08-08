6300995ff0f7173e6855e9756eebcfade7c82546
Alexis Thibault <alexis.thibault31@gmail.com>
2025-08-08 12:35:32 +0200

    :sparkles: structure settings view

--- lib/src/pages/settings/view.dart
+++ lib/src/pages/settings/view.dart
@@ -1,14 +1,54 @@
+import 'package:app/l10n/app_localizations.dart';
+import 'package:app/src/pages/settings/widgets/accounts_form.dart';
+import 'package:app/src/pages/settings/widgets/categories_form.dart';
+import 'package:app/src/shared/widgets/tabs/swipe_tabs.dart';
 import 'package:flutter/material.dart';
+import 'widgets/preferences_form.dart';
+import 'view_model.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    ThemeData theme = Theme.of(context);
+  State<SettingsPage> createState() => _SettingsPageState();
+}
 
-    return Center(
-      child: Text('Settings Page', style: theme.textTheme.headlineMedium),
+class _SettingsPageState extends State<SettingsPage> {
+  late final SettingsViewModel _viewModel;
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _viewModel = SettingsViewModel();
+  }
+
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    AppLocalizations tr = AppLocalizations.of(context)!;
+
+    return SwipeTabs(
+      initialIndex: _currentIndex,
+      onIndexChanged: (index) => setState(() => _currentIndex = index),
+      tabs: [
+        Text(tr.accounts),
+        Text(tr.categories),
+        Text(tr.preferences),
+      ],
+      children: [
+        AccountsForm(accounts: _viewModel.accounts.items, onAdd: _viewModel.accounts.add, onEdit: _viewModel.accounts.update, onDelete: _viewModel.accounts.remove),
+        CategoriesForm(categories: _viewModel.categories.items, onAdd: _viewModel.categories.add, onEdit: _viewModel.categories.update, onDelete: _viewModel.categories.remove),
+        PreferencesForm(
+            currentThemeMode: _viewModel.theme.mode,
+            changeTheme: _viewModel.theme.change,
+          )
+      ],
     );
   }
 }
