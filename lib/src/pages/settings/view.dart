d9b7c9a64ad64ad21904ef2745f3a3a2d2f35ab4
Alexis Thibault <alexis.thibault31@gmail.com>
2025-08-30 14:46:27 +0200

    :recycle: Update Settings page

--- lib/src/pages/settings/view.dart
+++ lib/src/pages/settings/view.dart
@@ -3,8 +3,9 @@ import 'package:app/src/pages/settings/widgets/accounts_form.dart';
 import 'package:app/src/pages/settings/widgets/categories_form.dart';
 import 'package:app/src/shared/widgets/tabs/swipe_tabs.dart';
 import 'package:flutter/material.dart';
-import 'widgets/preferences_form.dart';
+
 import 'view_model.dart';
+import 'widgets/preferences_form.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -31,24 +32,47 @@ class _SettingsPageState extends State<SettingsPage> {
 
   @override
   Widget build(BuildContext context) {
-    AppLocalizations tr = AppLocalizations.of(context)!;
+    final tr = AppLocalizations.of(context)!;
 
-    return SwipeTabs(
-      initialIndex: _currentIndex,
-      onIndexChanged: (index) => setState(() => _currentIndex = index),
-      tabs: [
-        Text(tr.accounts),
-        Text(tr.categories),
-        Text(tr.preferences),
-      ],
-      children: [
-        AccountsForm(accounts: _viewModel.accounts.items, onAdd: _viewModel.accounts.add, onEdit: _viewModel.accounts.update, onDelete: _viewModel.accounts.remove),
-        CategoriesForm(categories: _viewModel.categories.items, onAdd: _viewModel.categories.add, onEdit: _viewModel.categories.update, onDelete: _viewModel.categories.remove),
-        PreferencesForm(
-            currentThemeMode: _viewModel.theme.mode,
-            changeTheme: _viewModel.theme.change,
-          )
-      ],
+    return AnimatedBuilder(
+      animation: _viewModel,
+      builder: (context, child) {
+        if (_viewModel.isLoading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        return SwipeTabs(
+          initialIndex: _currentIndex,
+          onIndexChanged: (index) => setState(() => _currentIndex = index),
+          tabs: [
+            Text(tr.accounts),
+            Text(tr.categories),
+            Text(tr.preferences),
+          ],
+          children: [
+            AccountsForm(
+              accounts: _viewModel.accounts.items,
+              onAdd: _viewModel.accounts.add,
+              onEdit: _viewModel.accounts.update,
+              onDelete: _viewModel.accounts.remove,
+            ),
+            CategoriesForm(
+              categories: _viewModel.categories.items,
+              onAdd: _viewModel.categories.add,
+              onEdit: _viewModel.categories.update,
+              onDelete: _viewModel.categories.remove,
+            ),
+            PreferencesForm(
+              currentThemeMode: _viewModel.preferences.mode,
+              currentLocale: _viewModel.preferences.locale,
+              changeTheme: _viewModel.preferences.changeTheme,
+              changeLocale: _viewModel.preferences.changeLocale,
+            ),
+          ],
+        );
+      },
     );
   }
 }
