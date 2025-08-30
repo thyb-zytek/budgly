fdcf295125f72048fa605e7264df181530a87d10
Alexis Thibault <alexis.thibault31@gmail.com>
2025-08-30 14:39:26 +0200

    :sparkles: Add locale modifier form

---
+++ lib/src/pages/settings/widgets/locale_form.dart
@@ -0,0 +1,62 @@
+import 'package:app/l10n/app_localizations.dart';
+import 'package:app/src/shared/widgets/inputs/dropdown.dart';
+import 'package:flutter/material.dart';
+
+class LocaleForm extends StatelessWidget {
+  final Locale currentLocale;
+  final ValueChanged<Locale> onLocaleChanged;
+
+  const LocaleForm({
+    required this.currentLocale,
+    required this.onLocaleChanged,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final tr = AppLocalizations.of(context)!;
+    final theme = Theme.of(context);
+
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 8, 8, 4),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            tr.locale,
+            style: theme.textTheme.headlineSmall!.copyWith(
+              color: theme.colorScheme.primary,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Container(
+              constraints: BoxConstraints(
+                maxWidth: MediaQuery.of(context).size.width * 0.5,
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                shape: BoxShape.rectangle,
+                color: theme.colorScheme.surfaceContainer,
+              ),
+              child: DropDown(
+                dense: true,
+                initialValue: currentLocale,
+                options: AppLocalizations.supportedLocales,
+                onSelect: onLocaleChanged,
+                optionBuilder:
+                    (option) => Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                      child: Text(option.languageCode.toUpperCase()),
+                    ),
+                ),
+              ),  
+            ),
+        ],
+      ),
+    );
+  }
+}
