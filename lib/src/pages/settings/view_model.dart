db207e4ed0c041c8ab4a55f31bfe0a15ec5ca94c
Alexis Thibault <alexis.thibault31@gmail.com>
2025-08-30 14:47:32 +0200

    :recycle: Update Settings viewModel

--- lib/src/pages/settings/view_model.dart
+++ lib/src/pages/settings/view_model.dart
@@ -1,15 +1,51 @@
 import 'package:app/src/models/account/account.dart';
 import 'package:app/src/models/category/category.dart';
-import 'package:app/src/services/theme_service.dart';
+import 'package:app/src/services/preferences_service.dart';
 import 'package:flutter/material.dart';
 
+class PreferencesViewModel extends ChangeNotifier {
+  final PreferencesService _preferencesService = PreferencesService();
+
+  ThemeMode get mode => _preferencesService.themeMode;
+  Locale get locale => _preferencesService.locale;
+  String get currency => _preferencesService.currency;
+
+  List<String> get supportedCurrencies => PreferencesService.supportedCurrencies;
+
+  Future<void> changeTheme(ThemeMode? themeMode) async {
+    if (themeMode != null && themeMode != _preferencesService.themeMode) {
+      await _preferencesService.setThemeMode(themeMode);
+      notifyListeners();
+    }
+  }
+
+  Future<void> changeLocale(Locale? locale) async {
+    if (locale != null && locale != _preferencesService.locale) {
+      await _preferencesService.setLocale(locale);
+      notifyListeners();
+    }
+  }
+
+  Future<void> changeCurrency(String? currency) async {
+    if (currency != null && currency != _preferencesService.currency) {
+      await _preferencesService.setCurrency(currency);
+      notifyListeners();
+    }
+  }
+
+  Future<void> loadInitialTheme() async {
+    notifyListeners();
+  }
+}
 
 abstract class BaseListViewModel<T> extends ChangeNotifier {
   final List<T> _items = [];
-  
+
   List<T> get items => List.unmodifiable(_items);
-  
-  T? retrieve(String id, String Function(T) idSelector) {
+
+  BaseListViewModel();
+
+  T? _retrieve(String id, String Function(T) idSelector) {
     try {
       return _items.firstWhere((item) => idSelector(item) == id);
     } catch (_) {
@@ -17,17 +53,17 @@ abstract class BaseListViewModel<T> extends ChangeNotifier {
     }
   }
 
-  Future<void> add(T item) async {
+  Future<void> _add(T item) async {
     _items.add(item);
     notifyListeners();
   }
 
-  Future<void> remove(String id, String Function(T) idSelector) async {
+  Future<void> _remove(String id, String Function(T) idSelector) async {
     _items.removeWhere((item) => idSelector(item) == id);
     notifyListeners();
   }
 
-  Future<void> update(
+  Future<void> _update(
     String id,
     T Function(T) updateFn,
     String Function(T) idSelector,
@@ -40,71 +76,45 @@ abstract class BaseListViewModel<T> extends ChangeNotifier {
   }
 }
 
-class ThemeViewModel extends ChangeNotifier {
-  final ThemeService _themeService = ThemeService();
-
-  ThemeMode get mode => _themeService.themeMode;
-
-  ThemeViewModel() {
-    _themeService.addListener(_onThemeChanged);
-  }
-
-  @override
-  void dispose() {
-    _themeService.removeListener(_onThemeChanged);
-    super.dispose();
-  }
-
-  void _onThemeChanged() => notifyListeners();
-
-  Future<void> change(ThemeMode? themeMode) async {
-    if (themeMode != null) {
-      await _themeService.setThemeMode(themeMode);
-      notifyListeners();
-    }
-  }
-}
-
 class AccountsViewModel extends BaseListViewModel<Account> {
-  Future<void> removeAccount(String id) => remove(id, (item) => item.id);
-  
-  Future<void> updateAccount(
-    String id,
-    Account updates,
-  ) => update(
-    id,
-    (item) => Account.fromJson({...item.toJson(), ...updates.toJson()}),
-    (item) => item.id,
-  );
+  AccountsViewModel() : super();
+
+  Account? retrieve(String id) => _retrieve(id, (item) => item.id);
+
+  Future<void> add(Account account) => _add(account);
+
+  Future<void> remove(String id) => _remove(id, (item) => item.id);
+
+  Future<void> update(String id, Account account) =>
+      _update(id, (item) => account.copyWith(), (item) => item.id);
 }
 
 class CategoriesViewModel extends BaseListViewModel<Category> {
-  Future<void> removeCategory(String id) => remove(id, (item) => item.id);
-  
-  Future<void> updateCategory(
-    String id,
-    Category updates,
-  ) => update(
-    id,
-    (item) => Category.fromJson({...item.toJson(), ...updates.toJson()}),
-    (item) => item.id,
-  );
+  CategoriesViewModel() : super();
+
+  Category? retrieve(String id) => _retrieve(id, (item) => item.id);
+
+  Future<void> add(Category category) => _add(category);
+
+  Future<void> remove(String id) => _remove(id, (item) => item.id);
+
+  Future<void> update(String id, Category category) =>
+      _update(id, (item) => category.copyWith(), (item) => item.id);
 }
 
-class SettingsViewModel extends ChangeNotifier {
-  final ThemeViewModel _theme = ThemeViewModel();
-  final AccountsViewModel _accounts = AccountsViewModel();
-  final CategoriesViewModel _categories = CategoriesViewModel();
+class SettingsViewModel with ChangeNotifier {
+  final PreferencesViewModel preferences = PreferencesViewModel();
+  final AccountsViewModel accounts = AccountsViewModel();
+  final CategoriesViewModel categories = CategoriesViewModel();
 
-  ThemeViewModel get theme => _theme;
-  AccountsViewModel get accounts => _accounts;
-  CategoriesViewModel get categories => _categories;
+  bool _isLoading = false;
+
+  bool get isLoading => _isLoading;
 
   @override
   void dispose() {
-    _theme.dispose();
-    _accounts.dispose();
-    _categories.dispose();
+    accounts.dispose();
+    categories.dispose();
     super.dispose();
   }
 }
