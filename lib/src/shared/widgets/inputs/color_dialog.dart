1cd0220e404f7787b00751c475ad9298b2ad109e
Alexis Thibault <alexis.thibault31@gmail.com>
2025-10-03 11:29:36 +0200

    :recycle: Update Color dialog widget

--- lib/src/shared/widgets/inputs/color_dialog.dart
+++ lib/src/shared/widgets/inputs/color_dialog.dart
@@ -1,5 +1,6 @@
+import 'package:app/l10n/app_localizations.dart';
+import 'package:app/src/shared/widgets/buttons/button.dart';
 import 'package:app/src/shared/widgets/buttons/constants.dart';
-import 'package:app/src/shared/widgets/buttons/icon_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
@@ -11,6 +12,7 @@ class ColorDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final tr = AppLocalizations.of(context)!;
     Color _selectedColor = color;
 
     return AlertDialog(
@@ -19,48 +21,62 @@ class ColorDialog extends StatelessWidget {
       shape: RoundedRectangleBorder(
         borderRadius:
             MediaQuery.of(context).orientation == Orientation.portrait
-                ? const BorderRadius.only(
-                  topLeft: Radius.circular(140),
-                  topRight: Radius.circular(10),
-                  bottomRight: Radius.circular(10),
-                  bottomLeft: Radius.circular(10),
+                ? const BorderRadius.vertical(
+                  top: Radius.circular(140),
+                  bottom: Radius.circular(10),
                 )
-                : const BorderRadius.horizontal(right: Radius.circular(500)),
+                : const BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(140),
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                ),
       ),
-      content: SingleChildScrollView(
-        child: Stack(
-          children: [
-            HueRingPicker(
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8).copyWith(
+              bottom: 0,
+              right:
+                  MediaQuery.of(context).orientation == Orientation.portrait
+                      ? 8
+                      : 16,
+            ),
+            child: HueRingPicker(
               pickerColor: _selectedColor,
               onColorChanged: (color) => _selectedColor = color,
               enableAlpha: false,
               displayThumbColor: true,
+              pickerAreaBorderRadius: BorderRadius.circular(10),
             ),
-            Positioned(
-              right: 0,
-              top: 0,
-              child: BudglyIconButton(
-                type: ButtonType.iconDefault,
-                smallIcon: true,
-                icon: Icons.close_rounded,
-                onPressed: () => Navigator.pop(context),
-              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                BudglyButton(
+                  text: tr.cancel,
+                  type: ButtonType.error,
+                  dense: true,
+                  onPressed: () => Navigator.pop(context),
+                ),
+                BudglyButton(
+                  text: tr.validate,
+                  type: ButtonType.success,
+                  dense: true,
+                  onPressed: () {
+                    onChange(_selectedColor);
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
             ),
-            Positioned(
-              right: 16,
-              bottom: 24,
-              child: BudglyIconButton(
-                type: ButtonType.success,
-                smallIcon: true,
-                icon: Icons.check_rounded,
-                onPressed: () {
-                  onChange(_selectedColor);
-                  Navigator.pop(context);
-                },
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
