bfec0b9898429e76db8bb9159a15209ff107cf1a
Alexis Thibault <alexis.thibault31@gmail.com>
2025-10-03 11:33:46 +0200

    :sparkles: Add extension to handle string hex color

---
+++ lib/src/core/extensions/color.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+
+extension HexColor on Color {
+  static Color fromHex(String hexString) {
+    final buffer = StringBuffer();
+    if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
+    buffer.write(hexString.replaceFirst('#', ''));
+    return Color(int.parse(buffer.toString(), radix: 16));
+  }
+
+  String toHex({bool leadingHashSign = true, bool includeAlpha = true}) {
+   return this.toHexString();
+  }
+}
