a0f9651685417b517089244e859b5099508cabbf
Alexis Thibault <alexis.thibault31@gmail.com>
2026-02-18 22:35:54 +0100

    :sparkles: add loading icons on start of application

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,7 @@
 import 'package:app/l10n/app_localizations.dart';
 import 'package:app/src/core/routers/base.dart';
 import 'package:app/src/core/theme/theme.dart';
+import 'package:app/src/services/category_icons.dart';
 import 'package:app/src/services/preferences.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -14,6 +15,8 @@ class BudglyApp extends StatefulWidget {
 
 class _BudglyAppState extends State<BudglyApp> {
   final PreferencesService _preferencesService = PreferencesService();
+  final CategoryIconsService _categoryIconsService =
+      CategoryIconsService.instance;
   late ThemeMode _currentThemeMode;
   late Locale _currentLocale;
 
@@ -21,6 +24,7 @@ class _BudglyAppState extends State<BudglyApp> {
   void initState() {
     super.initState();
     PreferencesService.init();
+    _categoryIconsService.getIcons();
     _currentThemeMode = _preferencesService.themeMode;
     _currentLocale = _preferencesService.locale;
     _preferencesService.addListener(_handleThemeChange);
